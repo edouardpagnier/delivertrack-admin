f7ff2fb103fe3cdb293223e13de4ad0b3bd51ffb
Edouard Pagnier <edouard.pagnier@gmail.com>
2016-08-23 12:07:24 +0700

    add CSS template and bootstrap css

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,5 @@
+
+
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+<link rel="stylesheet" href="{{ asset('css/spinners.css') }}">
